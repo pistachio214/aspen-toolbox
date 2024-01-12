a571bbfa604f6fe81143e78f9c6fc7f61e3ac93d
Songyang <aileshang0226@163.com>
2024-01-13 00:29:33 +0800

    update 完善ssh登录流程

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::path::Path;
 use clap::{ArgMatches, Command};
+use colored::Colorize;
 use prettytable::{format, row, Table};
-use ssh2::PtyModes;
+use ssh2::{PtyModes};
 use {
     ssh2::Session,
     std::{
@@ -29,7 +30,7 @@ fn build_cli() -> Command {
         .name("aspen toolbox")
         .version("0.0.1")
         .author("Aspen Soung<songyang420@outlook.com>")
-        .about("尝试写的一个由Rust开发的命令行工具")
+        .about("Aspen工具箱")
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(build_ssh_toolbox())
@@ -38,7 +39,7 @@ fn build_cli() -> Command {
 // 构建ssh工具的命令详情
 fn build_ssh_toolbox() -> Command {
     Command::new("ssh")
-        .about("打印服务器列表")
+        .about("ssh工具箱")
     // .args([
     //     Arg::new("input").help("输入的是啥").required(true),
     //     Arg::new("name").help("输入名称").required(true)
@@ -47,14 +48,17 @@ fn build_ssh_toolbox() -> Command {
 
 // 试试这个实现
 fn impl_ssh_action(_: &ArgMatches) {
+    // 清屏
+    clear_terminal();
+
     let file = "./config.ini";
 
-    // 1. 读取服务器配置数据列表
+    // 读取服务器配置数据列表
     if Path::new(&file).exists() {
         match File::open(file) {
             Ok(mut f) => {
                 let mut data = String::new();
-                f.read_to_string(&mut data).expect("[Aspen Error] => 无法读取文件.");
+                f.read_to_string(&mut data).expect("\n[Aspen Error] => 无法读取文件.");
 
                 let lines: Vec<&str> = data.lines().collect();
                 let mut new_lines: Vec<Vec<String>> = Vec::new();
@@ -78,7 +82,7 @@ fn impl_ssh_action(_: &ArgMatches) {
                 let mut config_selected: Vec<Vec<String>> = Vec::new();
 
                 if !&new_lines.is_empty() {
-                    println!("请输入 序号 选择要登录的服务器:");
+                    println!("请输入 {} 选择要登录的服务器:", "序号".green());
                     loop {
                         let mut guess = String::new();
                         io::stdin().read_line(&mut guess).expect("读取输入错误");
@@ -86,14 +90,14 @@ fn impl_ssh_action(_: &ArgMatches) {
                         let guess: i32 = match guess.trim().parse() {
                             Ok(num) => {
                                 if num > new_lines.len() as i32 {
-                                    eprintln!("您输入的序号超过了配置项的数量!");
+                                    eprintln!("\n[Aspen Error] => {} 请重新输入服务器 {}:", "您输入的序号超过了配置项的数量!".red(), "序号".green());
                                     continue;
                                 }
 
                                 num
                             }
                             Err(_) => {
-                                eprintln!("序号只能输入合法的整数!");
+                                eprintln!("\n[Aspen Error] => {} 请重新输入服务器 {}:", "序号只能输入合法的整数!".red(), "序号".green());
                                 continue;
                             }
                         };
@@ -116,25 +120,44 @@ fn impl_ssh_action(_: &ArgMatches) {
             }
         }
     } else {
-        eprintln!("[Aspen Error] => 您的配置文件不存在 💔");
+        eprintln!("\n [Aspen Error] => {}\n", "您的配置文件不存在 💔".red());
         process::exit(1);
     }
 }
 
 fn ssh_login(config: Vec<String>) {
-    println!("选中的配置是: {:?}", config);
-
     let connect = format!("{}:{}", config[0].clone(), config[2].clone());
     let username = config[3].clone();
     let password = config[1].clone();
+    let title = config[4].clone();
+
+    println!("\n[Aspen Waiting] ==> 正在登录【 {} 】，请稍等...", title.green());
 
     let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(TcpStream::connect(connect).unwrap());
+    match TcpStream::connect(connect) {
+        Ok(tcp) => {
+            sess.set_tcp_stream(tcp);
+        }
+        Err(_) => {
+            eprintln!("\n[Aspen Error] => {}\n", "链接超时,请检查您的网络是否通畅或者您的Host信息是否正确".red());
+            process::exit(1);
+        }
+    }
+
     sess.handshake().unwrap();
-    sess.userauth_password(&username, &password).unwrap();
+
+    match sess.userauth_password(&username, &password) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("\n[Aspen Error] => {}\n", e.message().red());
+            process::exit(1);
+        }
+    }
+
+    // sess.userauth_password(&username, &password).unwrap();
 
     let mut pty_modes = PtyModes::new();
-    pty_modes.set_boolean(ssh2::PtyModeOpcode::ECHO,false); //关闭回显
+    pty_modes.set_boolean(ssh2::PtyModeOpcode::ECHO, false); //关闭回显
     pty_modes.set_boolean(ssh2::PtyModeOpcode::IGNCR, true); //忽略输入的回车
 
     let mut channel = sess.channel_session().unwrap();
@@ -152,7 +175,9 @@ fn ssh_login(config: Vec<String>) {
             ssh_stdin.write_all(&buf[..size]).unwrap();
         }
     });
+
     let stdout_thread = thread::spawn(move || {
+        print!("\n {} \n", "Login Successful!!!".green());
         loop {
             let mut buf = [0; 1024];
             match channel.read(&mut buf) {
@@ -165,12 +190,13 @@ fn ssh_login(config: Vec<String>) {
             }
         }
 
-
-        channel.close().unwrap();
-        print!("Exit: {}", channel.exit_status().unwrap());
+        let exit_status = channel.exit_status().unwrap();
+        if exit_status == 0 {
+            println!("\n[Aspen Success] ==> 您已退出【 {} 】\n", title.green());
+            channel.close().unwrap();
+            process::exit(1);
+        }
     });
-
-    // let status = sess.wait_status().unwrap();
 
     stdin_thread.join().unwrap();
     stdout_thread.join().unwrap();
@@ -208,6 +234,12 @@ fn print_services_table(lines: Vec<Vec<String>>) {
 
     // 打印表格到标准输出
     table.printstd();
+}
+
+//清屏
+fn clear_terminal() {
+    print!("\x1b[2J");
+    print!("\x1b[H");
 }
 
 fn error_action() {
