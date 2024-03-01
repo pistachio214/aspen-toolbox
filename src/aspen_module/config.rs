b515fb4a80e016556e31b9aae1bb582189cb7d21
Songyang <aileshang0226@163.com>
2024-03-02 00:40:49 +0800

    update version 0.1.20

--- src/aspen_module/config.rs
+++ src/aspen_module/config.rs
@@ -17,47 +17,42 @@ pub struct Config {
  * 获取Aspen的配置文件地址
  */
 pub fn get_aspen_config() -> Config {
-    if let Some(home_dir) = dirs::home_dir() {
-        let dir = env!("CARGO_PKG_NAME");
-        let config_dir = format!("{}/{}/config", get_home_dir().to_str().unwrap().to_string(), dir.to_string());
+    let dir = env!("CARGO_PKG_NAME");
+    let config_dir = format!("{}/{}/config", get_home_dir().to_str().unwrap().to_string(), dir.to_string());
 
-        generate_folder(config_dir.clone());
+    generate_folder(config_dir.clone());
 
-        // 命令指定的配置文件位置
-        let config_path = format!("{}/aspen_config.json", config_dir);
+    // 命令指定的配置文件位置
+    let config_path = format!("{}/aspen_config.json", config_dir);
 
-        //配置文件存在就直接读取,反之则创建
-        if Path::new(&config_path).exists() {
-            // 打开文件
-            read_config(&PathBuf::from(config_path.as_str())).unwrap()
-        } else {
-            // 创建一个 Config 结构体实例
-            let config = Config {
-                service_config_path: "".to_string(),
-            };
+    //配置文件存在就直接读取,反之则创建
+    if Path::new(&config_path).exists() {
+        // 打开文件
+        read_config(&PathBuf::from(config_path.as_str())).unwrap()
+    } else {
+        // 创建一个 Config 结构体实例
+        let config = Config {
+            service_config_path: "".to_string(),
+        };
 
-            // 将 Config 结构体序列化为 JSON 格式的字符串
-            let json_string: String = match serde_json::to_string_pretty(&config) {
-                Ok(s) => s,
-                Err(_) => {
-                    eprintln!("\n[Aspen Error] => {} \n", "错误配置信息!".red());
-                    std::process::exit(0);
-                }
-            };
-
-            if let Ok(mut file) = File::create(&config_path) {
-                file.write_all(json_string.as_bytes()).unwrap();
-                // println!("成功创建并写入 JSON 文件: {:?}", config_path);
-
-                config
-            } else {
-                eprintln!("\n[Aspen Error] => {} \n", "无法创建配置文件!".red());
+        // 将 Config 结构体序列化为 JSON 格式的字符串
+        let json_string: String = match serde_json::to_string_pretty(&config) {
+            Ok(s) => s,
+            Err(_) => {
+                eprintln!("\n[Aspen Error] => {} \n", "错误配置信息!".red());
                 std::process::exit(0);
             }
+        };
+
+        if let Ok(mut file) = File::create(&config_path) {
+            file.write_all(json_string.as_bytes()).unwrap();
+            // println!("成功创建并写入 JSON 文件: {:?}", config_path);
+
+            config
+        } else {
+            eprintln!("\n[Aspen Error] => {} \n", "无法创建配置文件!".red());
+            std::process::exit(0);
         }
-    } else {
-        eprintln!("\n[Aspen Error] => {} \n", "无法获取用户主目录!".red());
-        std::process::exit(0);
     }
 }
 
